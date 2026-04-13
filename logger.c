2eabd691efe8a51c3d563390886d5cd67db44213
ksenera <kushikasenera@gmail.com>
2026-04-13 00:25:37 -0400

    build(logger): buffer init for 10 prompt questions

---
+++ logger.c
@@ -0,0 +1,19 @@
+/*
+ * includes printf, fgets, fopen, fprintf, fclose
+ * time(), ctime() 
+ * strlen() strip newline from ctime() output
+ */
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+int main() 
+{
+    // filename path to output md in obsidian vault
+    char filename[200];
+    // reused for each answer to 10 log encode/transfer prompts
+    char buffer[500];
+
+
+    return 0;
+}
